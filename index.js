a61bacb346b89b7aa88cccd66865f2e804a00eda
padster06 <paddy2006@hotmail.co.uk>
2022-04-16 17:34:52 +0100

    server selects a random selction of cards

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const cardsRaw = fs.readFileSync("./lib/cards.json");
+const cards = JSON.parse(cardsRaw);
+console.log(cards.white);
 
 const Room = require("./lib/room").Room;
 const Player = require("./lib/player").Player;
@@ -10,27 +15,53 @@ const port = 3000;
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-let rooms = [];
+const genID = () => {
+  let num = 0;
+  for (let i = 0; i < 5; i++) {
+    num += Math.floor(Math.random() * 10) * 10 ** i;
+  }
+  num += 10000;
+  if (num > 99999) {
+    num -= 10000;
+  }
+  return num;
+};
+
+const assignCards = () => {
+  let arr = [];
+  for (let i = 0; i < 5; i++) {
+    arr.push(cards.white[Math.floor(Math.random() * cards.white.length)]);
+  }
+  console.log(arr);
+  return arr;
+};
+
+let rooms = [new Room("temp", genID())];
+console.log(rooms);
 app.use("/", express.static("public"));
 
 app.post("/api/join", jsonParser, (req, res) => {
-    console.log(req.body);
-    for (let i = 0; i < rooms.length; i++) {
-        if (rooms[i].id == req.body.roomID) {
-            rooms[i].players.push(new Player(req.body.usrName, Date.now()));
-            res.send(
-                JSON.stringify({
-                    roomJoined: true,
-                    roomName: rooms[i].name,
-                    playerID: rooms[i].players[rooms[i].players.length - 1].id,
-                })
-            );
-        }
+  console.log(req.body);
+  for (let i = 0; i < rooms.length; i++) {
+    if (rooms[i].id == req.body.roomID) {
+      console.log(true);
+      rooms[i].players.push(new Player(req.body.usrName, Date.now()));
+      rooms[i].players[rooms[i].players.length - 1].cards = assignCards();
+      res.send(
+        JSON.stringify({
+          roomJoined: true,
+          roomName: rooms[i].name,
+          playerID: rooms[i].players[rooms[i].players.length - 1].id,
+          playerObj: rooms[i].players[rooms[i].players.length - 1],
+        })
+      );
+      console.log(rooms[0].players);
     }
+  }
 });
 
 app.listen(port, () => {
-    console.log(`app listening on http://localhost:3000`);
+  console.log(`app listening on http://localhost:3000`);
 });
 
 rooms.push(new Room("test", 123, "me"));
--- public/main.js
+++ public/main.js
@@ -4,119 +4,130 @@ let blackBackIMG = new Image();
 let whiteBackIMG = new Image();
 let blackFrontIMG = new Image();
 let whiteFontIMG = new Image();
+let initInfo;
 
 function wrapText(context, text, x, y, maxWidth, lineHeight) {
-    var words = text.split(" ");
-    var line = "";
+  var words = text.split(" ");
+  var line = "";
 
-    for (var n = 0; n < words.length; n++) {
-        var testLine = line + words[n] + " ";
-        var metrics = context.measureText(testLine);
-        var testWidth = metrics.width;
-        if (testWidth > maxWidth && n > 0) {
-            context.fillText(line, x, y);
-            line = words[n] + " ";
-            y += lineHeight;
-        } else {
-            line = testLine;
-        }
+  for (var n = 0; n < words.length; n++) {
+    var testLine = line + words[n] + " ";
+    var metrics = context.measureText(testLine);
+    var testWidth = metrics.width;
+    if (testWidth > maxWidth && n > 0) {
+      context.fillText(line, x, y);
+      line = words[n] + " ";
+      y += lineHeight;
+    } else {
+      line = testLine;
     }
-    context.fillText(line, x, y);
+  }
+  context.fillText(line, x, y);
 }
 
 function init() {
-    // blackBackIMG = new Image();
-    blackBackIMG.src = "img/blackBack.png";
-    whiteBackIMG.src = "img/whiteBack.png";
-    blackFrontIMG.src = "img/blackFront.png";
-    whiteFontIMG.src = "img/whiteFront.png";
+  // blackBackIMG = new Image();
+  blackBackIMG.src = "img/blackBack.png";
+  whiteBackIMG.src = "img/whiteBack.png";
+  blackFrontIMG.src = "img/blackFront.png";
+  whiteFontIMG.src = "img/whiteFront.png";
 
-    joinHTLM = document.getElementById("join-room");
-    playingRoomHTML = document.getElementById("playing-room");
-    canvas = document.getElementById("playing-canvas");
-    display = canvas.getContext("2d");
-    canvas.width = 800;
-    canvas.height = 600;
-    game();
-    joinHTLM.style.display = "none";
-    // playingRoomHTML.style.display = "none";
-    document
-        .getElementById("join-dim")
-        .addEventListener("click", (e) => e.preventDefault());
+  joinHTLM = document.getElementById("join-room");
+  playingRoomHTML = document.getElementById("playing-room");
+  canvas = document.getElementById("playing-canvas");
+  display = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 600;
+  //   requestAnimationFrame(game);
+  joinHTLM.style.display = "none";
+  // playingRoomHTML.style.display = "none";
+  document
+    .getElementById("join-dim")
+    .addEventListener("click", (e) => e.preventDefault());
 }
 
 function game() {
-    display.fillStyle = "#fff";
-    display.fillRect(0, 0, canvas.width, canvas.height);
-    display.fillStyle = "rgb(33, 37, 41)";
-    display.fillRect(2, 2, canvas.width - 4, canvas.height - 4);
+  //   console.log("loop");
+  display.fillStyle = "#fff";
+  display.fillRect(0, 0, canvas.width, canvas.height);
+  display.fillStyle = "rgb(33, 37, 41)";
+  display.fillRect(2, 2, canvas.width - 4, canvas.height - 4);
 
-    //  BLACK CARDS
+  //  BLACK CARDS
 
-    display.drawImage(
-        blackBackIMG,
-        canvas.width / 2 - 120,
-        canvas.height / 2 + 60,
-        120,
-        200
-    );
+  display.drawImage(
+    blackBackIMG,
+    canvas.width / 2 - 120,
+    canvas.height / 2 + 60,
+    120,
+    200
+  );
 
-    display.drawImage(
-        blackFrontIMG,
-        canvas.width / 2 + 20,
-        canvas.height / 2 + 60,
-        120,
-        200
-    );
+  display.drawImage(
+    blackFrontIMG,
+    canvas.width / 2 + 20,
+    canvas.height / 2 + 60,
+    120,
+    200
+  );
 
-    //  WHITE CARDS
+  //  WHITE CARDS
+  if (initInfo.playerObj) {
     for (let i = 0; i < 5; i++) {
-        const x = (canvas.width / 5) * i + (canvas.width / 5 - 120) / 2;
-        display.drawImage(whiteFontIMG, x, 100, 120, 200);
-        display.font = "18px sans-serif";
-        display.fillStyle = "#000000";
-        wrapText(display, "", x + 10, 120, 120, 25);
+      const x = (canvas.width / 5) * i + (canvas.width / 5 - 120) / 2;
+      display.drawImage(whiteFontIMG, x, 100, 120, 200);
+      display.font = "18px sans-serif";
+      display.fillStyle = "#000000";
+      wrapText(display, initInfo.playerObj.cards[i], x + 10, 120, 115, 25);
     }
+  }
+  requestAnimationFrame(game);
 }
 
 function uHideJoin() {
-    joinHTLM.style.display = "block";
+  joinHTLM.style.display = "block";
 }
 
 async function joinRoom() {
+  let usrName = document.getElementById("join-username").value;
+  let roomID = document.getElementById("join-room-id").value;
+  if (roomID && usrName) {
     joinHTLM.style.display = "none";
-    let usrName = document.getElementById("join-username").value;
-    let roomID = document.getElementById("join-room-id").value;
+
     let data = await postData(window.location.href + "api/join", {
-        usrName,
-        roomID,
+      usrName,
+      roomID,
     });
     if (data.roomJoined) {
-        playingRoomHTML.style.display = "block";
-        canvas = document.getElementById("playing-canvas");
-        display = canvas.getContext("2d");
-        canvas.width = 800;
-        canvas.height = 600;
-        setInterval(game);
-        game();
+      playingRoomHTML.style.display = "block";
+      canvas = document.getElementById("playing-canvas");
+      display = canvas.getContext("2d");
+      canvas.width = 800;
+      canvas.height = 600;
+      // setInterval(game);
+      // game();
     }
     console.log(data);
+    initInfo = data;
+    //  console.log(initInfo.playerObj.cards);
+    game();
+  }
 }
 
 async function postData(url = "", data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json",
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
 }
