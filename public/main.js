f51b4bc8f66110ab69d1e9a7bf0e131f9f19b235
padster06 <paddy2006@hotmail.co.uk>
2022-04-16 15:45:03 +0100

    before starting 16/4

--- public/main.js
+++ public/main.js
@@ -1,11 +1,45 @@
 let joinHTLM, playingRoomHTML;
 let display, canvas;
+let blackBackIMG = new Image();
+let whiteBackIMG = new Image();
+let blackFrontIMG = new Image();
+let whiteFontIMG = new Image();
+
+function wrapText(context, text, x, y, maxWidth, lineHeight) {
+    var words = text.split(" ");
+    var line = "";
+
+    for (var n = 0; n < words.length; n++) {
+        var testLine = line + words[n] + " ";
+        var metrics = context.measureText(testLine);
+        var testWidth = metrics.width;
+        if (testWidth > maxWidth && n > 0) {
+            context.fillText(line, x, y);
+            line = words[n] + " ";
+            y += lineHeight;
+        } else {
+            line = testLine;
+        }
+    }
+    context.fillText(line, x, y);
+}
 
 function init() {
+    // blackBackIMG = new Image();
+    blackBackIMG.src = "img/blackBack.png";
+    whiteBackIMG.src = "img/whiteBack.png";
+    blackFrontIMG.src = "img/blackFront.png";
+    whiteFontIMG.src = "img/whiteFront.png";
+
     joinHTLM = document.getElementById("join-room");
     playingRoomHTML = document.getElementById("playing-room");
+    canvas = document.getElementById("playing-canvas");
+    display = canvas.getContext("2d");
+    canvas.width = 800;
+    canvas.height = 600;
+    game();
     joinHTLM.style.display = "none";
-    playingRoomHTML.style.display = "none";
+    // playingRoomHTML.style.display = "none";
     document
         .getElementById("join-dim")
         .addEventListener("click", (e) => e.preventDefault());
@@ -16,6 +50,33 @@ function game() {
     display.fillRect(0, 0, canvas.width, canvas.height);
     display.fillStyle = "rgb(33, 37, 41)";
     display.fillRect(2, 2, canvas.width - 4, canvas.height - 4);
+
+    //  BLACK CARDS
+
+    display.drawImage(
+        blackBackIMG,
+        canvas.width / 2 - 120,
+        canvas.height / 2 + 60,
+        120,
+        200
+    );
+
+    display.drawImage(
+        blackFrontIMG,
+        canvas.width / 2 + 20,
+        canvas.height / 2 + 60,
+        120,
+        200
+    );
+
+    //  WHITE CARDS
+    for (let i = 0; i < 5; i++) {
+        const x = (canvas.width / 5) * i + (canvas.width / 5 - 120) / 2;
+        display.drawImage(whiteFontIMG, x, 100, 120, 200);
+        display.font = "18px sans-serif";
+        display.fillStyle = "#000000";
+        wrapText(display, "", x + 10, 120, 120, 25);
+    }
 }
 
 function uHideJoin() {
@@ -36,6 +97,7 @@ async function joinRoom() {
         display = canvas.getContext("2d");
         canvas.width = 800;
         canvas.height = 600;
+        setInterval(game);
         game();
     }
     console.log(data);
